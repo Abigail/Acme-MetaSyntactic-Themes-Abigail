6d9ad1ceb852f0aecdbd716a2fc5306500cf11e2
Abigail <abigail@abigail.be>
2012-06-02 01:53:43 +0200

    New theme: kodokan

--- lib/Acme/MetaSyntactic/Themes/Abigail.pm
+++ lib/Acme/MetaSyntactic/Themes/Abigail.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 no  warnings 'syntax';
 
-our $VERSION = '2012060103';
+our $VERSION = '2012060201';
 
 
 1;
@@ -22,8 +22,8 @@ This package installs a few more themes for C<< Acme::MetaSyntactic >>.
 
 Currently, the following themes are supplied: C<< beatles >>, C<< bee_gees >>,
 C<< charlies_angels >>, C<< cluedo >>, C<< compass >>, C<< evangelist >>, 
-C<< famous_five >>, C<< jelly_belly >>, C<< michelin >>, C<< nehi >>,
-C<< noughts_and_crosses >>, C<< shou_dou_qi >>, C<< sodor >>, and
+C<< famous_five >>, C<< jelly_belly >>, C<< kodokan >>, C<< michelin >>,
+C<< nehi >>, C<< noughts_and_crosses >>, C<< shou_dou_qi >>, C<< sodor >>, and
 C<< willy_wonka >>.
 
 =head1 TODO
